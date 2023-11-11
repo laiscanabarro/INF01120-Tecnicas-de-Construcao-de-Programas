25623e4335fb1b8c601d0054000cec50c841ecd2
Laís Canabarro <81448189+laiscanabarro@users.noreply.github.com>
2023-11-11 19:31:32 +0000

    add new methods

--- lab4/poligonos/TrianguloEquilatero.java
+++ lab4/poligonos/TrianguloEquilatero.java
@@ -2,8 +2,24 @@ package lab4.poligonos;
 
 /**
  * TrianguloEquilatero
+ * Subclasse de Triangulo
  */
-public class TrianguloEquilatero {
+public class TrianguloEquilatero extends Triangulo {
 
-    
+    public TrianguloEquilatero(double lado1, double lado2, double lado3) {
+        super(lado1, lado2, lado3);
+    }
+
+    @Override
+    public double calculaArea() {
+        double lado = getLado1();
+        double  area = ((Math.pow(lado, 2)*Math.cbrt(3))/4); // area do triangulo equilatero (lado²√3)/4
+        return area;
+    }
+
+    @Override 
+    public void imprimeTipoPoligono() {
+        System.out.println("Triângulo Equilátero");
+    }
+
 }
